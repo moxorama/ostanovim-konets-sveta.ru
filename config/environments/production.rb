c5e8f56b54922b3ee94aea641cabe44f1dcf4c47
George Khromchenko <moxorama@yandex.ru>
2012-12-08 01:08:33 +0400

    Убрано сжатие assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ StopEndOfTheWorldRu::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
